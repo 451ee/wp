fe89364f1888bf0bbf2d946114e897c62d2e889b
Martin Sookael <martinsookael@gmail.com>
2013-06-14 18:00:05 +0300

    temporary fix for forkMe

--- 451.ee/header.php
+++ 451.ee/header.php
@@ -53,7 +53,7 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 	<div class="container">
-        <a href="https://github.com/451ee/wp"><img style="position: absolute; top: 0; right: 0; border: 0;" src="<?php echo get_template_directory_uri(); ?>/img/forkMe.png" alt="Fork me on GitHub"></a>		
+        <a href="https://github.com/451ee/wp"><img style="position: absolute; top: 28px; right: 0; border: 0; " src="<?php echo get_template_directory_uri(); ?>/img/forkMe.png" alt="Fork me on GitHub"></a>		
         <div id="header">
 		<a href="/"><img src="<? bloginfo('template_url');?>/img/logo.png" class="logo" /></a>
 
