5b9645e068140437051c7bc4d0a2a83feff56d05
Martin Sookael <martinsookael@gmail.com>
2013-06-15 01:28:43 +0300

    added 404

---
+++ 451.ee/404.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+
+			<article id="post-0" class="post error404 no-results not-found">
+				<header class="entry-header">
+					<h1 class="entry-title">Error 404 - Ei ole sellist lehte</h1>
+				</header>
+
+				<div class="entry-content">
+					<p>Aga siin on üks lahe video:</p>
+                    <iframe width="853" height="480" src="http://www.youtube.com/embed/ZG38VcjE770" frameborder="0" allowfullscreen></iframe>
+					<p>Avaldati see video vanameistri 90 sünnipäeva paiku, siin on näha tema reaktsioon:</p>
+                    <img src="http://images1.wikia.nocookie.net/__cb20100827215423/wikiality/images/a/a9/Ray-bradbury-watches-fuck-me-ray-bradbury.jpg"><br />
+                    <img src="http://images.dangerousminds.net/uploads/images/20ventura600_thumb.jpg"><br />
+                    allikas:<br>
+                    <a href="http://wikiality.wikia.com/File:Ray-bradbury-watches-fuck-me-ray-bradbury.jpg"></>http://wikiality.wikia.com/File:Ray-bradbury-watches-fuck-me-ray-bradbury.jpg</a><br />
+                    <a href="http://dangerousminds.net/comments/fuck_me_ray_bradbury"></>http://dangerousminds.net/comments/fuck_me_ray_bradbury</div>a><br />
+                    
+                    
+                    
+                </div><!-- .entry-content -->
+			</article><!-- #post-0 -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
