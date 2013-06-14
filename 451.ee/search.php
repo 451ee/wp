20e7fb18a269ef7b0b72bf9e3601ab0a370203cb
live <m@451.ee>
2013-06-15 01:29:40 +0300

    added search.php for search results

---
+++ 451.ee/search.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The template for displaying Search Results pages.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+<div id="content" class="clearfix row-fluid">
+	<section id="primary" class="site-content">
+		<div id="content" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<h1 class="page-title"><?php printf( __( 'Otsingu tulemused märksõnale: %s', 'twentytwelve' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+			</header>
+
+		
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+					<section class="post_content clearfix">
+					<?php the_content( __("Read more &raquo;","theme") ); ?>
+					</section>
+			<?php endwhile; ?>
+
+
+
+		<?php else : ?>
+
+			<article id="post-0" class="post no-results not-found">
+				<header class="entry-header">
+					<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentytwelve' ); ?></h1>
+				</header>
+
+				<div class="entry-content">
+					<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentytwelve' ); ?></p>
+				</div><!-- .entry-content -->
+			</article><!-- #post-0 -->
+
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</section><!-- #primary -->
+
+</div>
+<?php get_footer(); ?>
