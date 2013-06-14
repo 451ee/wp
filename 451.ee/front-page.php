1e334f430f04400c95452d71ab1f968932f9cbd8
webguru <webguru@c197-78.uvn.zone.eu>
2013-06-14 19:46:34 +0300

    Svens work

--- 451.ee/front-page.php
+++ 451.ee/front-page.php
@@ -1,45 +1,45 @@
 <?php get_header(); ?>
 <div id="content" class="clearfix row-fluid">
 	<!-- Carousel -->
-<div id="myCarousel" class="carousel slide"> 
-	<ol class="carousel-indicators">
-		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		<li data-target="#myCarousel" data-slide-to="1"></li>
-	</ol>
+<div id="myCarousel" class="carousel slide">
 	<div class="carousel-inner">
 		<?php
 			global $post;
 			$tmp_post = $post;
-			#$show_posts = of_get_option('slider_options');
 			$args = array( 'post_type' => 'feat', 'post_status' => 'publish', ); // set this to how many posts you want in the carousel
 			$myposts = get_posts( $args );
 			$post_num = 0;
 			foreach( $myposts as $post ) :	setup_postdata($post);
-			$post_num++;
-			#$post_thumbnail_id = get_post_thumbnail_id();
-			#$featured_src = wp_get_attachment_image_src( $post_thumbnail_id, 'wpbs-featured-carousel' );
-			?>
+			$post_num++; 
+		?>
 			<div class="<?php if($post_num == 1){ echo 'active'; } ?> item">
-			<?php the_post_thumbnail( 'full' ); ?>
-			<h2><?php the_title(); ?></h2>
-			<div class="feat_content">
-			<?php the_excerpt(); ?>
-			
-			<a href="" class="read_more"> <?php _e("Loe lähemalt", "theme"); ?> <i class="icon-chevron-right icon-large"></i></a>
+				<div class="feat_thumb">
+					<?php the_post_thumbnail( 'full' ); ?>
+				</div>
+				<div class="feat_content">
+					<h2><?php the_title(); ?></h2>
+					<?php the_excerpt(); ?>				
+					<a href="" class="read_more"> <?php _e("Loe lähemalt", "theme"); ?> <i class="icon-chevron-right icon-large"></i></a>
+					
+				</div>
+				<div class="clearfix"></div>
 			</div>
-		</div>
 	<?php endforeach; ?>
 	<?php $post = $tmp_post; ?>
-
-		</div>
+	</div>
+	<div class="indicator-position">
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+		</ol>
+	</div>
 </div><!-- /.carousel -->
-
 <div class="well">
 	<ul class="widgets">
-		<li><h3>2012 Raport</h3>
-<p><img src="http://451.ee/wp-content/uploads/icon-video.png" /> Videot<br />
-
-</p>
+		<li><h3>2012 Eelraport</h3>
+			<p><img src="http://451.ee/wp-content/uploads/icon-video.png" /> 22524 muusikapala</p>
+			<p><img src="http://451.ee/wp-content/uploads/icon-post.png" /> 82172 videot</p>
+			<p><img src="http://451.ee/wp-content/uploads/icon-site.png" /> 314 mängu</p>
 		</li>
 		<li><h3 class="muted">2013 Raport</h3></li>
 		<li><h3 class="muted">2014 Raport</h3></li>
